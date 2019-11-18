77fa086bbffbb024230e6e66d865c0b845a03ae8
mesneym <mesney.mesney@gmail.com>
2019-11-18 11:31:39 -0500

    Added Comments to files

--- include/modturtlebot.h
+++ include/modturtlebot.h
@@ -1,3 +1,43 @@
+/******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2019 Akwasi A Obeng 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+/**
+ * @file Robot.cpp
+ *
+ * @author  Akwasi A Obeng(Navigator)
+ *
+ * @brief This program allows the turtlebot to sense and avoid 
+ *        obstacles
+ *
+ * @version 1
+ *
+ * @date 2019-18-12
+ *
+ * This .h file has declarations for the class attributes and methods for
+ * simple functionality of the sensing and avoiding obstacles for turtebot.
+ */
+
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
 #include <ros/ros.h>
@@ -7,17 +47,57 @@
 
 class TurtleBot {
  private:
-   ros::NodeHandle n;
-   ros::Publisher pub;
-   ros::Subscriber sub;
-   geometry_msgs::Twist msg;
-   bool obstacle;
-   void scanEnvCallBack(const sensor_msgs::LaserScan::ConstPtr& msg);
+  ros::NodeHandle n; // Sets up the node
+
+  ros::Publisher pub; // Needed to publish to topics
+
+  ros::Subscriber sub; // Needed to subcribe to topics
+
+  geometry_msgs::Twist msg; // linear and angular velocities
+                            // Needed to move turtlebot
+  bool obstacle; // Flag to check whether object is near obstacle
+  float thresdist; // Threshold distance for obstacles
+
+    /**
+     * @brief Callback method after subscribing to callback message 
+     *        Scans envirnoment and checks it turtlebot is near to
+     *        wall 
+     *
+     * @param A pointer with reference to subsrcibed messages
+     * 
+     * @return None
+     *
+     */
+  void scanEnvCallBack(const sensor_msgs::LaserScan::ConstPtr& msg);
 
 
  public:
+    /**
+     * @brief Constructor to set initial values
+     * 
+     * @param None
+     * 
+     * @return None
+     *
+     */
     TurtleBot();
-    ~TurtleBot();
-    void moveTurtleBot();
 
+    /**
+     * @brief Destructor to destroy turtebot object
+     * 
+     * @param None
+     * 
+     * @return None
+     *
+     */
+    ~TurtleBot();
+    /**
+     * @brief method moves the turtlebot and avoids obstacles
+     * 
+     * @param None
+     * 
+     * @return None
+     *
+     */
+    void moveTurtleBot();
 };
--- src/modturtle.cpp
+++ src/modturtle.cpp
@@ -1,49 +1,74 @@
+/******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2019 Akwasi A Obeng 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
 #include "modturtlebot.h"
 
 TurtleBot::TurtleBot() {
+  // Setup publisher to publish to advertise topic to move turtlebot
   pub = n.advertise <geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1000);
-  sub = n.subscribe("/scan",1000,&TurtleBot::scanEnvCallBack,this);
+
+  // Setup subscriber to subscriber to scan obstacles
+  sub = n.subscribe("/scan", 1000, &TurtleBot::scanEnvCallBack, this);
+
+  thresdist = 1.5; // setting threshold dist
 }
 
-TurtleBot::~TurtleBot() {
-
-}
+TurtleBot::~TurtleBot() {}
 
 void TurtleBot::scanEnvCallBack(const sensor_msgs::LaserScan::ConstPtr& msg) {
   for (int i = 0; i< msg->ranges.size(); ++i) {
-    if (msg->ranges[i] < 1.5)
+    if (msg->ranges[i] < thresdist) {
        obstacle = true;
        return;
+    }
   }
   obstacle = false;
   return;
 }
 
+
+
 void TurtleBot::moveTurtleBot() {
-  ros::Rate loop_rate(10); 
+  ros::Rate loop_rate(10); // publish rate
 
   while (ros::ok()) {
-    pub.publish(msg);
-
-    //std_msgs::String m;
-    //std::stringstream ss;
-
+    pub.publish(msg); // publish message
+    /* check if there is an  obstacle do the following
+    *  Stop, Rotate about z axis, and then Move 
+    */
     if (obstacle) {
-      msg.angular.z = 1;
-      msg.linear.x = 0;
+      msg.linear.x = 0; // Stop
+      msg.angular.z = 1;  // Rotate about z axis
 
+      // Set obstacle to false to indicate it is okay to move
       obstacle = false;
     } else {
+      // Translate in x direction with no rotation
       msg.linear.x = 0.5;
       msg.angular.z = 0;
     }
-   
-    //m.data= ss.str();
-    //ROS_INFO_STREAM(""<<m.data.c_str());
-
-    //std::cout<< obstacle << std::endl;
     ros::spinOnce();
-
     loop_rate.sleep();
   }
 }
